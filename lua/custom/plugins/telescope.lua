866de5b942c2e1a05eb488f2d8e7a51b877f90c5
Christian Piet <43993129+Manbearpiet@users.noreply.github.com>
2026-04-08 17:14:18 +0200

    feat(nvim): ✨ improve treesitter config and telescope hidden file search
    * Migrate treesitter to new branch = 'main' API with explicit config function
    * Add jsonc parser to ensure_installed list
    * Remove deprecated main field and fold option from treesitter setup
    * Configure telescope vimgrep_arguments to include hidden files via --hidden flag with .git/ exclusion
    * Add markdown ftplugin enabling treesitter-based folding expressions

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -60,6 +60,19 @@ return { -- Fuzzy Finder (files, lsp, etc)
   },
   config = function()
     require('telescope').setup {
+      defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+          '--glob=!.git/',
+        },
+      },
       extensions = {
         ['ui-select'] = { require('telescope.themes').get_dropdown() },
       },
